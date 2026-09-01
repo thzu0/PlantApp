ae0d97caf86cd59b9f5d7e207824314ab717e545
thzu0 <taha.n.j01511@gmail.com>
2026-09-01 12:54:36 +0330

    add some page

--- lib/screens/root.dart
+++ lib/screens/root.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
-
 ///==================================
 ///this page is for const app bar and const nav
 ///==================================
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: const Text('RootPage')));
+  }
+}
